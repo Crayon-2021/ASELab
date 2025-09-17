03e80bc2f15c32043056793fc5ac026d47ab2dfc
liangyufeng <2085462714@qq.com>
2025-09-17 13:13:01 +0800

    Initial commit

---
+++ src/main/java/com/group6/aselab/AseLabApplication.java
@@ -0,0 +1,13 @@
+package com.group6.aselab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AseLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/aselab/AseLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.aselab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
