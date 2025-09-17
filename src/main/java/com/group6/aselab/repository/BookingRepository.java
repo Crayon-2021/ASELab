f4c2434386a115aa41c31d79a8283ea246de77ae
liangyufeng <2085462714@qq.com>
2025-09-17 22:42:11 +0800

    feat: 新增Repository层接口BookingRepository

---
+++ src/main/java/com/group6/aselab/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.group6.aselab.repository;
+
+import com.group6.aselab.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+}
